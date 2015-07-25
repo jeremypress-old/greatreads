ad2b063c25032ab26e528765f6f03ec2a3bb56b2
jeremypress <jeremypress@berkeley.edu>
2015-07-24 23:27:49 -0700

    added height switching and transitions

--- myreads.js
+++ myreads.js
@@ -2,40 +2,47 @@ var USER_ID = "32376874";
 var DEV_KEY = "4jzSxoAEeuI6HcD0HSCgjA";
 
 var url = "http://www.goodreads.com/review/list/" + USER_ID + "?format=xml&key=" + DEV_KEY + "&v=2";
-
+var numItems = 8;
 
 $( document ).ready(function() {
-    console.log( "ready!" );
-    console.log(url)
+
+    //API Call, Using yahoo to avoid CORS issues.
     var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + url + '"') + '&format=xml&callback=?';
-  $.getJSON(yql, function(data){
-    console.log(data.results[0]);
-    parseXML(data.results[0]);
-  });
+    $.getJSON(yql, function(data){
+        parseXML(data.results[0]);
+    });
 
-  var numItems = 3;
-$(document).ready(function() {
-  for (i = 0; i < numItems; i++) {
-    $(".myreads").append("<div class='myreads-item'>"
-      + "<img class='cover' src='#' />"
-      + "<p class='book-title'> </p>"
-      + "<p class ='author'> </p>"
-      + "<div class='rating'> </div>"
-      + "<div class='read'> </div>"
-      + "</div>");
-  }
-  $(".myreads-item").css("height", (70/(numItems - 1)) + "%");
-  $(".myreads-item:first-child").css("height", 30 + "%");
+    //Building the myreads items.
+    for (i = 0; i < numItems; i++) {
+        $(".myreads").append("<div class='myreads-item' id='item-" + i + "'>"
+            + "<img class='cover' src='#' />"
+            + "<p class='book-title'> </p>"
+            + "<p class ='author'> </p>"
+            + "<div class='rating'> </div>"
+            + "<div class='read'> </div>"
+            + "</div>");
+    }
 
-});
+    //Set the initial css, the first item is selected.
+    setSelectedItem('item-0');
+    $(".myreads-item").click(myreadsItemClick);
 });
 
 function parseXML(data) {
-  xmlDoc = $.parseXML(data),
-  $xml = $(xmlDoc),
-  $title = $xml.find("book").children('title').text();
-  console.log($title);
+    xmlDoc = $.parseXML(data),
+    $xml = $(xmlDoc),
+    $title = $xml.find("book").children('title').text();
+    console.log($title);
 }
 
-site = 'http://goo.gl/9iQWyG';
+function setSelectedItem(itemNumber) {
+    $(".myreads-item").css("height", (70/(numItems - 1)) + "%");
+    $('#' + itemNumber).css("height", 30 + "%");
+}
+
+function myreadsItemClick(event) {
+    var clickedId = this.id;
+    setSelectedItem(clickedId);
+}
+
 
